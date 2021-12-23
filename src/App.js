017329a578737e7ee1e9da1cc8e727fe699d412f
mohammad baqer kohie <mbk2019.co@gmail.com>
2021-12-23 09:16:06 +0330

    git init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+      <h1>Todo List</h1>
       </header>
+      <div className='todo-app'>
+        <div className='add-todo'>
+          <input type="text" placeholder='what need to do?'/>
+          <button type='submit'>Add</button>
+        </div>
+        <div className='todo-list'>
+          <ul>
+            <li>
+              <input type="checkbox"/>
+              <h2>Build thodo app</h2>
+              <button >delete</button>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <div>
+            2 items
+          </div>
+          <div>
+            <ul>
+              <li>
+                <button>Add</button>
+              </li>
+              <li>
+                <button>Active</button>
+              </li>
+              <li>
+                <button>Completed</button>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <footer>
+        <h3>develope by mbk</h3>
+      </footer>
     </div>
   );
 }
